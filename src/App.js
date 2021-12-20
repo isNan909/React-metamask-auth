bfcda2d91123d9bd0570dfd2647ef2754ce5d632
ishan01 <ishanmanandhar@lftechnology.com>
2021-12-20 20:50:32 +0545

    added a simple auth with metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Web3 from "web3";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [connectedAccount, setConnectedAccount] = useState(null);
+  const [connectedBalance, setConnectedBalance] = useState(null);
+  const [connectedId, setConnectedId] = useState(null);
+
+  const detectCurrentProvider = () => {
+    let provider;
+    if (window.ethereum) {
+      provider = window.ethereum;
+    } else if (window.web3) {
+      // eslint-disable-next-line
+      provider = window.web3.currentProvider;
+    } else {
+      console.log(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
+    }
+    return provider;
+  };
+
+  const onConnect = async () => {
+    try {
+      const currentProvider = detectCurrentProvider();
+      if (currentProvider) {
+        if (currentProvider !== window.ethereum) {
+          console.log(
+            'Non-Ethereum browser detected. You should consider trying MetaMask!'
+          );
+        }
+        await currentProvider.request({ method: 'eth_requestAccounts' });
+        const web3 = new Web3(currentProvider);
+        const userAccount = await web3.eth.getAccounts();
+        const chainId = await web3.eth.getChainId();
+        let ethBalance = await web3.eth.getBalance(userAccount[0]); // Get wallet balance
+        ethBalance = web3.utils.fromWei(ethBalance, 'ether'); //Convert balance to wei
+        if (userAccount.length === 0) {
+          console.log('please connect to meta mask');
+        } else if (userAccount[0] !== connectedAccount) {
+          setIsConnected(true);
+          setConnectedAccount(userAccount[0]);
+          setConnectedBalance(ethBalance);
+          setConnectedId(chainId);
+        }
+      }
+    } catch (err) {
+      console.log(
+        'There was an error fetching your accounts. Make sure your Ethereum client is configured correctly.'
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      React dApp authentication with Web3.js and Metamask
+      <div>
+        <button onClick={onConnect}>Connect to MetaMask</button>
+      </div>
+      {isConnected && (
+        <div>
+          You are connected to metamask. 
+          <br />
+          Balance: {connectedBalance}
+          <br />
+          ConnectionID: {connectedId}
+          <br />
+          Connected Account: {connectedAccount}
+        </div>
+      )}
     </div>
   );
 }
