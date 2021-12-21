3d728820e55618a5d4af3afe6de2097c92f74314
ishan01 <ishanmanandhar@lftechnology.com>
2021-12-21 21:06:24 +0545

    added local storage to save and persist user info

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Web3 from 'web3';
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
-  const [userinfo, setUserinfo] = useState({
-    account: '',
-    balance: '',
-    connctionid: '',
-  });
+  const [userInfo, setUserInfo] = useState({});
 
   const detectCurrentProvider = () => {
     let provider;
@@ -37,17 +33,12 @@ function App() {
         const web3 = new Web3(currentProvider);
         const userAccount = await web3.eth.getAccounts();
         const chainId = await web3.eth.getChainId();
-        let ethBalance = await web3.eth.getBalance(userAccount[0]); // Get wallet balance
+        const account = userAccount[0];
+        let ethBalance = await web3.eth.getBalance(account); // Get wallet balance
         ethBalance = web3.utils.fromWei(ethBalance, 'ether'); //Convert balance to wei
+        saveUserInfo(ethBalance, account, chainId);
         if (userAccount.length === 0) {
-          console.log('please connect to meta mask');
-        } else if (userAccount[0] !== userinfo.account) {
-          setIsConnected(true);
-          setUserinfo({
-            account: userAccount,
-            balance: ethBalance,
-            connctionid: chainId,
-          });
+          console.log('Please connect to meta mask');
         }
       }
     } catch (err) {
@@ -57,23 +48,25 @@ function App() {
     }
   };
 
+  const saveUserInfo = (ethBalance, account, chainId) => {
+    const userAccount = {
+      account: account,
+      balance: ethBalance,
+      connectionid: chainId,
+    };
+    window.localStorage.setItem('userAccount', JSON.stringify(userAccount)); //or use a state management lib
+    const userData = JSON.parse(localStorage.getItem('userAccount'));
+    setUserInfo(userData);
+    setIsConnected(true);
+  };
+
   return (
     <div className="App">
       React dApp authentication with Web3.js and Metamask
       <div>
         <button onClick={onConnect}>Connect to MetaMask</button>
       </div>
-      {isConnected && (
-        <div>
-          You are connected to metamask.
-          <br />
-          Balance: {userinfo.account}
-          <br />
-          ConnectionID: {userinfo.balance}
-          <br />
-          Connected Account: {userinfo.connctionid}
-        </div>
-      )}
+      {isConnected && <div>You are connected to metamask.{userInfo.account}, {userInfo.balance}, {userInfo.connectionid}</div>}
     </div>
   );
 }
